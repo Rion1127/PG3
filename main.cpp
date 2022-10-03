9790c0e07a831f9284bfeb07ed661f513f4f0dd0
Desctop <User@DESKTOP-FAGQ0KA>
2022-10-03 11:45:41 +0900

    修正

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 
-// 再帰的な時給
+// 再帰的な賃金
 int RecursiveWage(int value,int hour) {
 	if (hour <= 0) {
 		return value;
@@ -9,7 +9,7 @@ int RecursiveWage(int value,int hour) {
 	
 	return (RecursiveWage(value * 2 - 50,hour - 1));
 }
-//一般的な時給
+//一般的な賃金
 int GeneralWage(int value, int hour) {
 	return value * hour;
 }
@@ -19,22 +19,22 @@ int main() {
 	int hour = 10;		//時間
 	int result;			//結果を代入
 
-	//一般的な時給を代入
+	//一般的な賃金を代入
 	result = GeneralWage(wage, hour);
-	printf("一般的な時給\n%d円\n", result);
-	//再帰的な時給を代入
+	printf("一般的な賃金\n%d円\n", result);
+	//再帰的な賃金を代入
 	result = RecursiveWage(100,hour);
-	printf("再帰的な時給\n%d円\n", result);
+	printf("再帰的な賃金\n%d円\n", result);
 
-	//一般的な時給のほうが高いとき
+	//一般的な賃金のほうが高いとき
 	if (RecursiveWage(100, hour) < GeneralWage(wage, hour))
 	{
-		printf("\n一般的な時給の方が儲かる\n");
+		printf("\n一般的な賃金の方が儲かる\n");
 	}
-	//再帰的な時給のほうが高い
+	//再帰的な賃金のほうが高い
 	else if (RecursiveWage(100, hour) > GeneralWage(wage, hour))
 	{
-		printf("\n再帰的な時給の方が儲かる\n");
+		printf("\n再帰的な賃金の方が儲かる\n");
 	}
 	else {
 		printf("\nどちらも一緒\n");
