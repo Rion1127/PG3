7ef67ffbd18fa0870acd8129456bf7ad772c0e2b
Desctop <User@DESKTOP-FAGQ0KA>
2022-10-03 11:40:00 +0900

    1_2完了

--- main.cpp
+++ main.cpp
@@ -1,31 +1,44 @@
 #include <stdio.h>
 
-template <typename T>
-T Min(T a, T b) {
-	if (a < b) return a;
-	if (b < a) return b;
-	return 0;
-}
-template <>
-char Min(char a, char b) {
-	return printf("数字以外は代入できません\n");
-}
 
-int Wage(int wage, int hour) {
-	int i = 0;
-	if(hour)
-
-	return (Wage(wage))
+// 再帰的な時給
+int RecursiveWage(int value,int hour) {
+	if (hour <= 0) {
+		return value;
+	}
+	
+	return (RecursiveWage(value * 2 - 50,hour - 1));
+}
+//一般的な時給
+int GeneralWage(int value, int hour) {
+	return value * hour;
 }
 
 int main() {
-	int wage = 1072;
-	int hour = 10;
+	int wage = 1072;	//一般的な時給
+	int hour = 10;		//時間
+	int result;			//結果を代入
 
+	//一般的な時給を代入
+	result = GeneralWage(wage, hour);
+	printf("一般的な時給\n%d円\n", result);
+	//再帰的な時給を代入
+	result = RecursiveWage(100,hour);
+	printf("再帰的な時給\n%d円\n", result);
 
-
-
-
+	//一般的な時給のほうが高いとき
+	if (RecursiveWage(100, hour) < GeneralWage(wage, hour))
+	{
+		printf("\n一般的な時給の方が儲かる\n");
+	}
+	//再帰的な時給のほうが高い
+	else if (RecursiveWage(100, hour) > GeneralWage(wage, hour))
+	{
+		printf("\n再帰的な時給の方が儲かる\n");
+	}
+	else {
+		printf("\nどちらも一緒\n");
+	}
 
 	return 0;
 }
