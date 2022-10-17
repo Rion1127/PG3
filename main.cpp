3b3d2101d2f6a6e40d8c5573dd79a8af35d383f3
Desctop <User@DESKTOP-FAGQ0KA>
2022-10-17 11:35:52 +0900

    修正

--- main.cpp
+++ main.cpp
@@ -3,46 +3,51 @@
 #include <random>
 
 
-typedef void (*PFunc)(int*);
+typedef void (*PFunc)(int);
 
-void callback1(int* s) {
-	printf("%d秒後に実行されるよ\n", *s);
+void callback1(int number) {
+	int randNum = 0;
+
+	randNum = rand() % 6 + 1;
+	printf("結果 : %d\n", randNum);
+	if (number % 2 == randNum % 2) {
+		printf("プレイヤー勝ち");
+	}
+	else {
+		printf("プレイヤー負け");
+	}
+
+
 }
 
-void setTimeout(PFunc p, int second) {
-	p(&second);
+void setTimeout(PFunc p, int second,int number) {
+	printf("%d秒後に実行されるよ\n", second);
 
 	Sleep(1000 * second);
+
+	p(number);
 }
 
 int main() {
 	srand(time(nullptr));
 
 	int number = 0;
-	int randNum = 0;
 
-	printf("数字を入力してください\n");
-	scanf_s("%d", &number);	//入力
-
+	printf("数字を入力してください\n0 : 偶数　1 : 奇数\n");
+	while (true) {
+		scanf_s("%d", &number);	//入力
+		//0か1以外を入力されたらもう一度入力させる
+		if (number != 0 && number != 1) {
+			printf("エラー\n0 : 偶数　1 : 奇数\n");
+		}
+		else {
+			break;
+		}
+	}
 	//3秒待つ
 	PFunc p;
 	p = callback1;
-	setTimeout(p, 3);	
-
-	while (true) {
-		//偶数の乱数を返す
-		if (number % 2 == 0) {
-			randNum = rand();
-			if (randNum % 2 == 0) break;	//偶数ならループを抜ける
-		}
-		//奇数の乱数を返す
-		else {
-			randNum = rand();
-			if (randNum % 2 == 1) break;	//奇数ならループを抜ける
-		}
-	}
-
-	printf("%d\n", randNum);
+	setTimeout(p, 3,number);
 
 	return 0;
 }
