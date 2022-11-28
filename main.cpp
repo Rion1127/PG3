15a41fe4b84f619320f7b6c14f7153ed5b7a44f0
Desctop <User@DESKTOP-FAGQ0KA>
2022-11-28 14:55:12 +0900

    修正

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@ enum menu {
 
 void Create(CELL* currentCELL, const char* buf);
 void Index(CELL* endCell);
+void Delete(CELL* currentCELL);
 
 int main() {
 	char str[8];
@@ -27,7 +28,7 @@ int main() {
 	head.next = nullptr;
 
 	while (true) {
-
+		//メニュー
 		if (selectMenu == menu::MENU) {
 			printf("\n[要素の操作]\n");
 			printf("1.要素の一覧表示\n");
@@ -35,19 +36,18 @@ int main() {
 			printf("3.最後尾の要素の削除\n");
 			printf("\n---------------------\n");
 			printf("操作を選択してください\n");
-			scanf_s("%d", &selectMenu);
 
-			//0か1以外を入力されたらもう一度入力させる
-			if (selectMenu < menu::DISPLAY_ && 
+			scanf_s("%d", &selectMenu);
+			
+			//表示されていない番号以外を入力されたらもう一度入力させる
+			if (selectMenu < menu::DISPLAY_ ||
 				selectMenu > menu::DELETE_)
 			{
-				break;
-			}
-			else {
 				printf("エラー\n表示されている番号を選択してください\n");
 				selectMenu = menu::MENU;
 			}
 		}
+		//表示
 		else if (selectMenu == menu::DISPLAY_) {
 			//リスト一覧の表示
 			Index(&head);
@@ -57,8 +57,14 @@ int main() {
 			printf("0.初期画面に戻る\n");
 			scanf_s("%d", &selectMenu);
 
+			//表示されていない番号以外を入力されたらもう一度入力させる
+			if (selectMenu != 0)
+			{
+				printf("エラー\n表示されている番号を選択してください\n");
+				selectMenu = menu::DISPLAY_;
+			}
 		}
-
+		//挿入
 		else if (selectMenu == menu::INSERT_) {
 			printf("\n[リスト要素の挿入]\n");
 			printf("\n追加する文字を入力してください\n");
@@ -70,8 +76,44 @@ int main() {
 			printf("\n---------------------\n");
 			printf("0.初期画面に戻る\n");
 			scanf_s("%d", &selectMenu);
-		}
 
+			//表示されていない番号以外を入力されたらもう一度入力させる
+			if (selectMenu != 0)
+			{
+				printf("エラー\n表示されている番号を選択してください\n");
+				selectMenu = menu::INSERT_;
+			}
+		}
+		//削除
+		else if (selectMenu == menu::DELETE_) {
+			int select = 0;
+			printf("\n[要素の操作]\n");
+			printf("1.最後尾の要素を削除する\n");
+			printf("2.初期画面に戻る\n");
+			scanf_s("%d", &select);
+			//削除する
+			if (select == 1) {
+
+				Delete(&head);
+
+				printf("最後尾の要素を削除しました\n");
+
+				printf("\n---------------------\n");
+				printf("0.初期画面に戻る\n");
+				scanf_s("%d", &selectMenu);
+
+				//表示されていない番号以外を入力されたらもう一度入力させる
+				if (selectMenu != 0)
+				{
+					printf("エラー\n表示されている番号を選択してください\n");
+					selectMenu = menu::INSERT_;
+				}
+			}
+			//メニューに戻る
+			else if (select == 2) {
+				selectMenu = menu::MENU;
+			}
+		}
 	}
 
 
@@ -104,9 +146,25 @@ void Index(CELL* endCell)
 	printf("要素一覧:{\n");
 	while (endCell->next != nullptr) {
 		endCell = endCell->next;
-		printf("%d: %s,\n", no,endCell->str);
+		printf("%d: %s,\n", no, endCell->str);
 		no++;
 	}
 	printf("}\n");
-	printf("\n要素数: %d\n",no);
+	printf("\n要素数: %d\n", no);
+}
+
+void Delete(CELL* currentCELL)
+{
+	//中身が入っている時
+	if (currentCELL->next) {
+		while (currentCELL->next->next != nullptr) {
+			//次のセルのアドレスを代入
+			currentCELL = currentCELL->next;
+		}
+	}
+
+	if (currentCELL->next != nullptr) {
+		delete currentCELL->next;
+		currentCELL->next = nullptr;
+	}
 }
